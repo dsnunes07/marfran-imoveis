2f94f93f1365bd239fba04e1fb9239ffb4f509b0
Daniel Silva Nunes <dsnunes@usp.br>
2020-02-10 13:24:26 -0300

    create admins table

---
+++ db/migrate/20200210161957_create_admins.rb
@@ -0,0 +1,9 @@
+class CreateAdmins < ActiveRecord::Migration[6.0]
+  def change
+    create_table :admins do |t|
+      t.string :name, null:false
+      t.string :email, null: false, unique: true
+      t.timestamps
+    end
+  end
+end
