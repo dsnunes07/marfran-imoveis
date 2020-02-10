f003604e6655d5cc51970fdb032f8ccb9d886dbd
Daniel Silva Nunes <dsnunes@usp.br>
2020-02-10 17:02:41 -0300

    devise configs

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :admins, controllers: { sessions: 'admins/sessions' }
+  authenticate :admin do
+    resources :admins, only: [:index]
+  end
 end
