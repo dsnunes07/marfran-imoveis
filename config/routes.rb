2c70f74c18cbed52018d45fe0a14e274ee9a5854
Daniel Silva Nunes <dsnunes@usp.br>
2020-02-10 22:20:38 -0300

    change routes and db

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  root 'application#index'
   devise_for :admins, controllers: { sessions: 'admins/sessions' }
   authenticate :admin do
     resources :admins, only: [:index]
+    resources :real_estate, only: [:new, :create, :edit, :update, :destroy]
   end
+  get 'real_estate/index', to: 'real_estate#index', as: :list_real_estate
+  get 'real_estate/:id', to: 'real_estate#show', as: :show_real_estate
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_10_190840) do
+ActiveRecord::Schema.define(version: 2020_02_10_202917) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,13 @@ ActiveRecord::Schema.define(version: 2020_02_10_190840) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  end
+
+  create_table "real_estates", force: :cascade do |t|
+    t.integer "real_estate_type", null: false
+    t.string "name", null: false
+    t.integer "rooms"
+    t.boolean "showing", default: true, null: false
   end
 
 end
