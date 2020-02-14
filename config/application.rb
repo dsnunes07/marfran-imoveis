a1e5fac5f923653f40712b2f34177c546d826981
Daniel Silva Nunes <dsnunes@usp.br>
2020-02-14 15:21:40 -0300

    wont load assets on precompile

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,7 @@ module Marfran
     # translation file
     config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
     config.i18n.default_locale = :br
+    config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
