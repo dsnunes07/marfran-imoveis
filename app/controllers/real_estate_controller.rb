87270b1d9ff29c0256e5c2f5acc86fa46ee3acf4
Daniel Silva Nunes <dsnunes@usp.br>
2020-02-14 13:17:03 -0300

    pagination on controller

--- app/controllers/real_estate_controller.rb
+++ app/controllers/real_estate_controller.rb
@@ -1,7 +1,7 @@
 class RealEstateController < ApplicationController
   layout 'admins'
   def index
-    @real_estates = RealEstate.all
+    @real_estates = RealEstate.order(:created_at).page(params[:page])
   end
 
   def new
@@ -13,6 +13,7 @@ class RealEstateController < ApplicationController
   end
 
   def create
+    binding.pry
     address = Address.new(address_params)
     if address.save
       real_estate = RealEstate.new(real_estate_params)
